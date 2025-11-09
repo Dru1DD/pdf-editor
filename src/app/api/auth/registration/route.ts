5da14a805530e5bdc718e2db24a4b5b3bf651b5d
Dru1DD <mrtproger@gmail.com>
2025-11-09 18:28:37 +0100

    pnpm format

--- src/app/api/auth/registration/route.ts
+++ src/app/api/auth/registration/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 import bcrypt from 'bcrypt';
 
-export const GET = () => { };
+export const GET = () => {};
 
 /**
  * @swagger
